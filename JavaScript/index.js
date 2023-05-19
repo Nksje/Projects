396be0f9a2f034416eca80b1d68742ef1e7534eb
Daniil Bondar <daniil.bondar49@gmail.com>
2023-05-19 16:37:20 +0300

    clock code changed

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -568,16 +568,12 @@ function update() {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    } else {
-      seconds = seconds;
-    }
     if (hours < 10) {
       hours = "0" + hours;
-    }
-    if (minutes < 10) {
+    } else if (minutes < 10) {
       minutes = "0" + minutes;
+    } else if (seconds < 10) {
+      seconds = "0" + seconds;
     }
 
     return `${hours}:${minutes}:${seconds}`;
